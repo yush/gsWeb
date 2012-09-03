c52beee63cc6c1bd0ecada2f8f18917c9af1417d
ourycl@gmail.com <ourycl@gmail.com>
2012-09-03 23:29:30 +0200

    bootstrap submit form lancement siteswap par form debut grille de saisie

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,11 +2,17 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+import play.data.validation.Constraints.Required;
 
 import views.html.*;
 
 public class Application extends Controller {
   
+  public static class Siteswap {
+	  @Required public String ssBase;
+  }
+	
   public static Result index() {
     return ok(index.render("Your new application is ready."));
   }
@@ -15,8 +21,22 @@ public class Application extends Controller {
 	  return ok(juggle.render());
   }
   
-  public static Result staticPage() {
-	  return ok(staticPage.render());
+  public static Result basicForm() {
+	  Form<Siteswap> siteswapForm = form(Siteswap.class); 
+	  return ok(basicForm.render(siteswapForm));
+  }
+  
+  public static Result responseBasicForm() {
+	  Form<Siteswap> siteswapForm = form(Siteswap.class).bindFromRequest();
+	  if ( !siteswapForm.hasErrors()) {
+		  return ok(responseBasicForm.render(siteswapForm));
+	  } else {
+		  return badRequest(basicForm.render(siteswapForm));
+	  }
+  }
+  
+  public static Result gsGrid() {
+	  return ok(gsGrid.render());
   }
   
 }
