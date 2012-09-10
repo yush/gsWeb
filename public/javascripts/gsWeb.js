22c3f30fd54a2e0107457d22bd190c3bbb5e2c73
clem <ourycl@gmail.com>
2012-09-11 01:04:19 +0200

    ajout gsSiteswap + js

---
+++ public/javascripts/gsWeb.js
@@ -0,0 +1,29 @@
+require(["dojo/ready", "dojo/dom-form", "dojo/dom", "dojo/on", "dojo/request/xhr", "dojo/domReady!"],
+function(ready, domForm, dom, on, xhr){
+	function gridToJson(){
+		var gridJson;
+		console.info("gridToJson");
+		gridJson = domForm.toJson("gridForm");
+		dom.byId("output").innerHTML = gridJson;
+		xhr("gsSiteswap", {
+			handleAs: "text/html",
+			data: gridJson,
+			method: "POST",
+			headers : {
+        		"Content-Type" : "application/json"   			
+   			}
+		}).then(function(response){
+			console.info("reponse:"+response);
+			dom.byId("jlabApplet").innerHTML = response;
+		}, function(err) {
+			console.info("Error:"+err);
+		});
+	};	
+		
+	ready(function(){
+		var convertForm;
+		console.info("ready");
+		convertForm = dom.byId("convertForm");
+		on(convertForm, "click", gridToJson);
+	});
+});
